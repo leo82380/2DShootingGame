b32a48c117bdb578d8428f8c93d4a22d7d599455
leo82380 <tkdrb062@gmail.com>
2023-05-15 23:09:08 +0900

    EnemyMove

--- Assets/Scripts/EnemyScripts/EnemeMove.cs
+++ Assets/Scripts/EnemyScripts/EnemeMove.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class EnemeMove : MonoBehaviour
 {
     [SerializeField] float enemySpeed = 7;
+    Vector3 dir;
+    Player_Controller player;
+    private void Awake()
+    {
+        player = FindObjectOfType<Player_Controller>();
+    }
+    private void Start()
+    {
+        int rd = Random.Range(0, 100);
+        if(rd < 30)
+        {
+            dir = player.transform.position - transform.position;
+        }
+        else
+        {
+            dir = Vector3.down;
+        }
+    }
     void Update()
     {
-        transform.position += Vector3.down * Time.deltaTime * enemySpeed;
-        if(transform.position.y <= -10)
+        transform.position += dir.normalized * Time.deltaTime * enemySpeed;
+        if (transform.position.y <= -10)
         {
             Destroy(gameObject);
         }
