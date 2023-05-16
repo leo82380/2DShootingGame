2f752b1af659f5e366e0492e768576ccfffdd58d
leo82380 <tkdrb062@gmail.com>
2023-05-16 14:25:32 +0900

    Enemyspawner update

--- Assets/Scripts/EnemyScripts/EnemySpwner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpwner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemySpwner : MonoBehaviour
 {
+    [SerializeField] GameObject[] enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class EnemySpwner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int i = Random.Range(0, enemy.Length);
+        GameObject a = Instantiate(enemy[i]);
     }
 }
